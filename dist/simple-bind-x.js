9f1c14902b49123e8818ba1538b96299b93f7a83
Graham Fairweather <xotic750@gmail.com>
2019-08-16 13:48:51 +0200

    :bookamark: v1.0.0

--- dist/simple-bind-x.js
+++ dist/simple-bind-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2019-present",
-  "date": "2019-08-16T11:44:51.286Z",
+  "date": "2019-08-16T11:47:13.212Z",
   "describe": "",
   "description": "Creates a new function with a bound sequence of arguments. Compliant to 8 arguments.",
   "file": "simple-bind-x.js",
